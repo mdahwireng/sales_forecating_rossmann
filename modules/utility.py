fd107066d3a962c0424b2f27a204fa1233342ea1
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-07-26 13:23:27 +0000

    Adds utility module

---
+++ modules/utility.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import dvc.api
+
+def get_csv_data(path, version):
+    rev=version
+    print('Reading data from dvc source....')
+    data_url = dvc.api.get_url(path=path, rev=rev)
+    df = pd.read_csv(data_url)
+    print('Reading completed, data saved as {}'.format(path))
+    return df
+
+def save_csv(df, path):
+    df.to_csv(path, index=False)
+    print('Data saved as {}'.format(path))
